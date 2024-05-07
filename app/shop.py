b7c479127a7c925c27bc315e230e7360494c6ac6
elysianz <fateofheart@gmail.com>
2024-05-07 17:02:10 +1200

    Add shop detail

--- app/shop.py
+++ app/shop.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route("/shop")
 def shop():
     return render_template('shop.html')
+
+
+@app.route("/shopdetail")
+def shopDetail():
+    return render_template('shop_detail.html')
