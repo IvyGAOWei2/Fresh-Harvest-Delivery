3ee2637337c3d5c609ae909607ab0edcc46d7bb2
elysianz <fateofheart@gmail.com>
2024-05-14 17:00:05 +1200

    Update employee profile update [backend]

--- app/admin.py
+++ app/admin.py
@@ -1,19 +1,48 @@
 from app import app
-from flask import render_template, session
+from flask import render_template, request, session
 
 # User-defined function
-from common import getUserProfile
+from dbFile.config import updateSQL
+from common import getUserProfile, validateEmployeeProfile
 
 
 @app.route("/admin")
 def admin():
-    profile = getUserProfile(session['id'], session['type'])
-    return render_template('admin.html', profile=profile)
+    return render_template('admin.html')
 
 @app.route("/profile/employee")
 def profileEmployee():
     profile = getUserProfile(session['id'], session['type'])
     return render_template('profile-employee.html', profile=profile)
+
+@app.route('/profile/update', methods=['POST'])
+def profileUpdate():
+    verified_data = validateEmployeeProfile(request.get_json())
+    user_id = verified_data['user_id']
+    verified_data.pop('user_id')
+
+    if session.get('type') in ['Consumer']:
+        table_name = "Consumer"
+        return {"status": False}, 500
+    elif session.get('type') in ['Staff', 'Local_Manager', 'National_Manager']:
+        table_name = "Employees"
+    else:
+        return {"status": False}, 500
+
+    if verified_data:
+        updates,params = [], []
+        for key, value in verified_data.items():
+            updates.append(f"{key} = %s")
+            params.append(value)
+        params.append(user_id)
+
+    update_successful = updateSQL("UPDATE " + table_name + " SET " + ", ".join(updates) + " WHERE user_id = %s", params)
+
+    if update_successful:
+        return {"status": True}, 200
+    else:
+        return {"status": False}, 500
+
 
 @app.route("/admin/action")
 def adminAction():
--- common.py
+++ common.py
@@ -13,6 +13,17 @@ class Login(BaseModel):
     password: str = Field(min_length=1, max_length=50)
     is_saved: Optional[str] = Field(None, min_length=1, max_length=1)
 
+# Model for employee profile
+class employeeProfile(BaseModel):
+    user_id: int = Field(ge=1, le=999)
+    given_name: Optional[str] = Field(None, min_length=1, max_length=35)
+    family_name: Optional[str] = Field(None, min_length=1, max_length=35)
+    address: Optional[str] = Field(None, min_length=1, max_length=80)
+    email: Optional[EmailStr] = Field(None)
+    phone: Optional[str] = Field(None, min_length=1, max_length=13)
+    hire_date: Optional[datetime] = Field(None)
+    depot_id: Optional[int] = Field(None, ge=1, le=10)
+
 
 def validateLogin(data):
     try:
@@ -23,6 +34,14 @@ def validateLogin(data):
 
 def validateUserAccount(email):
     return fetchOne('SELECT * FROM Users WHERE email = %s AND is_deleted = FALSE', (email,), True)
+
+def validateEmployeeProfile(data):
+    try:
+        profile = employeeProfile(**data)
+        return profile.model_dump(exclude_none=True)
+    except ValidationError as e:
+        # print(e.errors())
+        return False
 
 def getUserProfile(id, type):
     user_table = {'Consumer': 'Consumer'}.get(type, 'Employees')
