0bc5db62806dac06de666e009fa4f623d0e138b5
HuizhongWang <minasopor23@gmail.com>
2024-05-16 19:46:38 +1200

    admin manage profile

--- app/admin_manage_profile.py
+++ app/admin_manage_profile.py
@@ -13,10 +13,17 @@ def admin_manage_profile(profile_type):
     if profile_type == 'Consumer':
         result = fetchAll("SELECT * FROM Consumer;")
         profile_type = 'Consumer'
+
     elif profile_type == 'Staff':
         result = fetchAll("""SELECT Employees.*, Depots.location FROM Employees 
                           join Users on Employees.user_id=Users.user_id 
                           join Depots on Employees.depot_id=Depots.depot_id where Users.type='Staff';""")
         profile_type = 'Staff'
+
+    elif profile_type == 'Local_Manager':
+        result = fetchAll("""SELECT Employees.*, Depots.location FROM Employees 
+                          join Users on Employees.user_id=Users.user_id 
+                          join Depots on Employees.depot_id=Depots.depot_id where Users.type='Local_Manager';""")
+        profile_type = 'Local manager'
 
     return render_template('admin_manage_profile.html', member_list=result,profile_type=profile_type)
