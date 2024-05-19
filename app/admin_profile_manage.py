c27da7d2356fe0ec4f54603358bb69cab68d028f
HuizhongWang <minasopor23@gmail.com>
2024-05-19 21:34:11 +1200

    user type filter

--- app/admin_profile_manage.py
+++ app/admin_profile_manage.py
@@ -30,27 +30,37 @@ def admin_profile_list(profile_type):
 
 
 @app.route('/admin/profile/search',methods = ["GET","POST"])
+# @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
 def profileSearch():
-    if session.get('type') != 3:
-        return {"status": False}, 500
 
     searchBy = request.get_json()['searchBy']
     profile_type = request.get_json()['profile_type']
 
     result = fetchAll("SELECT * FROM " + profile_type + " WHERE first_name LIKE %s \
         OR last_name LIKE %s ORDER BY user_id ASC", ('%' + searchBy + '%','%' + searchBy + '%'))
-    return render_template('profile_search.html', member_list=result, profile_type=profile_type)
+    return render_template('admin_profile_list.html', member_list=result, profile_type=profile_type)
 
 
 @app.route("/admin/profile/update",methods = ["GET","POST"])
 # @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
-def admin_profile_update(profile_type):
-    print(request.form.get("member_email",9999999999999999999999999999))
+def admin_profile_update():
+    if request.method == 'POST':   
+        if request.values.get("update_member") == "update_member":
+            print(request.form.get("member_new_password",9999999999999999999999999999))
+    return render_template('admin_profile_list.html')
+    
+
+@app.route("/admin/profile/add",methods = ["GET","POST"])
+# @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
+def admin_profile_add():
+    if request.values.get("add_member") == "add_member":
+        print(request.form.get("add_password",888888888888888888888) )
 
     return render_template('admin_profile_list.html')
 
 
 @app.route("/admin/profile/delete",methods = ["GET","POST"])
 # @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
-def admin_profile_delete(profile_type):
+def admin_profile_delete():
     pass
+    return render_template('admin_profile_list.html')
