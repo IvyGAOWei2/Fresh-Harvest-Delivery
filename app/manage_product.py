d8fb904e1dd4721f82c60cc5a5b1feb7f7d4668b
Wei Gao <Wei.Gao2@lincolnuni.ac.nz>
2024-05-19 19:20:28 +1200

    validation and delist product

--- app/manage_product.py
+++ app/manage_product.py
@@ -6,7 +6,7 @@ from dbFile.config import fetchAll, fetchOne,updateSQL
 from dbFile.config import updateSQL
 from common import roleRequired, getUserProfile, validateEmployeeProfile
 
-@app.route("/employee/products")
+@app.route("/employee/products", methods = ["GET","POST"])
 @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
 def manageProduct():
     sql_products = """
@@ -31,3 +31,82 @@ def manageProduct():
     product_list = fetchAll(sql_products)
     # print(product_list)
     return render_template('manage-products.html', productList = product_list)
+
+
+@app.route("/product/add",methods = ["GET","POST"])
+@roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
+def addProduct():
+    sql_products = """
+        SELECT 
+            p.product_id,
+            p.name,
+            p.description,
+            p.price,
+            p.stock,
+            c.category_name AS category,
+            u.unit_name AS unit,
+            d.location AS depot
+        FROM 
+            Products p
+        INNER JOIN 
+            Category c ON p.category_id = c.category_id
+        INNER JOIN 
+            Unit u ON p.unit_id = u.unit_id
+        INNER JOIN 
+            Depots d ON p.depot_id = d.depot_id;
+    """
+    product_list = fetchAll(sql_products)
+    return render_template('manage-products.html', productList = product_list)
+
+
+@app.route("/product/update", methods = ["GET","POST"])
+@roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
+def updateProduct():
+    sql_products = """
+        SELECT 
+            p.product_id,
+            p.name,
+            p.description,
+            p.price,
+            p.stock,
+            c.category_name AS category,
+            u.unit_name AS unit,
+            d.location AS depot
+        FROM 
+            Products p
+        INNER JOIN 
+            Category c ON p.category_id = c.category_id
+        INNER JOIN 
+            Unit u ON p.unit_id = u.unit_id
+        INNER JOIN 
+            Depots d ON p.depot_id = d.depot_id;
+    """
+    product_list = fetchAll(sql_products)
+    return render_template('manage-products.html', productList = product_list)
+
+
+
+@app.route("/product/delist/<int:product_id>", methods = ["GET","POST"])
+@roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
+def delistProduct(product_id):
+    sql_products = """
+        SELECT 
+            p.product_id,
+            p.name,
+            p.description,
+            p.price,
+            p.stock,
+            c.category_name AS category,
+            u.unit_name AS unit,
+            d.location AS depot
+        FROM 
+            Products p
+        INNER JOIN 
+            Category c ON p.category_id = c.category_id
+        INNER JOIN 
+            Unit u ON p.unit_id = u.unit_id
+        INNER JOIN 
+            Depots d ON p.depot_id = d.depot_id;
+    """
+    product_list = fetchAll(sql_products)
+    return render_template('manage-products.html', productList = product_list, )
