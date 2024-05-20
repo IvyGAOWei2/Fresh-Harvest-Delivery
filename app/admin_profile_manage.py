45a731dc881bfc3033cd0246bde2678d9a32d234
HuizhongWang <minasopor23@gmail.com>
2024-05-20 15:17:04 +1200

    test

--- app/admin_profile_manage.py
+++ app/admin_profile_manage.py
@@ -29,38 +29,39 @@ def admin_profile_list(profile_type):
     return render_template('admin_profile_list.html', member_list=result,profile_type=profile_type)
 
 
-@app.route('/admin/profile/search',methods = ["GET","POST"])
-# @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
-def profileSearch():
+# @app.route('/admin/profile/search',methods = ["GET","POST"])
+# # @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
+# def profileSearch():
 
-    searchBy = request.get_json()['searchBy']
-    profile_type = request.get_json()['profile_type']
+#     searchBy = request.get_json()['searchBy']
+#     profile_type = request.get_json()['profile_type']
 
-    result = fetchAll("SELECT * FROM " + profile_type + " WHERE first_name LIKE %s \
-        OR last_name LIKE %s ORDER BY user_id ASC", ('%' + searchBy + '%','%' + searchBy + '%'))
-    return render_template('admin_profile_list.html', member_list=result, profile_type=profile_type)
+#     result = fetchAll("SELECT * FROM " + profile_type + " WHERE first_name LIKE %s \
+#         OR last_name LIKE %s ORDER BY user_id ASC", ('%' + searchBy + '%','%' + searchBy + '%'))
+#     return render_template('admin_profile_list.html', member_list=result, profile_type=profile_type)
 
 
-@app.route("/admin/profile/list/update",methods = ["GET","POST"])
+@app.route("/admin/profile/update",methods = ["GET","POST"])
 # @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
 def admin_profile_update():
     if request.method == 'POST':   
     #     if request.values.get("update_member") == "update_member":
-        print(request.form.get("member_new_password",9999999999999999999999999999))
+        test = request.form.get("member_new_password")
+        print(request.form.get(test ,9999999999999999999999999999))
     return render_template('admin_profile_list.html')
     
 
-@app.route("/admin/profile/add",methods = ["GET","POST"])
-# @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
-def admin_profile_add():
-    if request.values.get("add_member") == "add_member":
-        print(request.form.get("add_password",888888888888888888888) )
+# @app.route("/admin/profile/add",methods = ["GET","POST"])
+# # @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
+# def admin_profile_add():
+#     if request.values.get("add_member") == "add_member":
+#         print(request.form.get("add_password",888888888888888888888) )
 
-    return render_template('admin_profile_list.html')
+#     return render_template('admin_profile_list.html')
 
 
-@app.route("/admin/profile/delete",methods = ["GET","POST"])
-# @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
-def admin_profile_delete():
-    pass
-    return render_template('admin_profile_list.html')
+# @app.route("/admin/profile/delete",methods = ["GET","POST"])
+# # @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
+# def admin_profile_delete():
+#     pass
+#     return render_template('admin_profile_list.html')
