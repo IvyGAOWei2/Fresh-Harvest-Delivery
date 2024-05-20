340bfe0c9ba1c7f4d5233dc1b24c345354cfeb4c
elysianz <fateofheart@gmail.com>
2024-05-20 18:25:16 +1200

    Update product del [backend]

--- app/manage_product.py
+++ app/manage_product.py
@@ -29,7 +29,9 @@ def manageProduct():
         INNER JOIN 
             Depots d ON p.depot_id = d.depot_id
         INNER JOIN 
-            ProductImages PI ON p.product_id = PI.product_id;
+            ProductImages PI ON p.product_id = PI.product_id
+        WHERE 
+            is_active = TRUE;
 """
     product_list = fetchAll(sql_products)
     # print(product_list)
@@ -78,27 +80,14 @@ def updateProduct():
 
 
 
-@app.route("/product/delist/<int:product_id>", methods = ["GET","POST"])
+@app.route("/product/delist", methods = ["POST"])
 @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
-def delistProduct(product_id):
-    sql_products = """
-        SELECT 
-            p.product_id,
-            p.name,
-            p.description,
-            p.price,
-            p.stock,
-            c.category_name AS category,
-            u.unit_name AS unit,
-            d.location AS depot
-        FROM 
-            Products p
-        INNER JOIN 
-            Category c ON p.category_id = c.category_id
-        INNER JOIN 
-            Unit u ON p.unit_id = u.unit_id
-        INNER JOIN 
-            Depots d ON p.depot_id = d.depot_id;
-    """
-    product_list = fetchAll(sql_products)
-    return render_template('manage-products.html', productList = product_list, )
+def productDelist():
+    data = request.get_json()
+
+    update_successful = updateSQL("UPDATE Products SET is_active = FALSE WHERE product_id = %s;", (data['product_id'],))
+
+    if update_successful:
+        return {"status": True}, 200
+    else:
+        return {"status": False}, 500
