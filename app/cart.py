909be834722b300d567b6a027073221024e80c0c
elysianz <fateofheart@gmail.com>
2024-05-30 18:43:47 +1200

    Fix add to cart

--- app/cart.py
+++ app/cart.py
@@ -4,13 +4,16 @@ import json
 
 # User-defined function
 from dbFile.config import insertSQL, updateSQL, fetchOne
+from common import roleRequired
 
 
 @app.route("/cart")
+@roleRequired(['Consumer', 'Staff', 'Local_Manager', 'National_Manager'])
 def cart():
     return render_template('cart.html')
 
 @app.route("/cart/update", methods = ["POST"])
+@roleRequired(['Consumer', 'Staff', 'Local_Manager', 'National_Manager'])
 def cartUpdate():
     data = json.dumps(request.get_json())
     try:
@@ -22,6 +25,7 @@ def cartUpdate():
 
 
 @app.route("/checkout")
+@roleRequired(['Consumer', 'Staff', 'Local_Manager', 'National_Manager'])
 def checkout():
     checkout_profile = fetchOne('SELECT given_name, family_name, address, postcode, phone FROM Consumer WHERE user_id = %s', (session['id'],), True)
     return render_template('chackout.html', checkoutProfile=checkout_profile)
