96b0fbb1ad5c463cebf361d77b867d840153ad81
Wei Gao <Wei.Gao2@lincolnuni.ac.nz>
2024-06-06 01:08:00 +1200

    giftcardsV1

--- app/manage_giftcards.py
+++ app/manage_giftcards.py
@@ -9,11 +9,26 @@ from common import roleRequired, validateProductProfile
 @app.route("/giftcards/list", methods = ["GET"])
 @roleRequired(['National_Manager'])
 def manageGiftcard():
-  
-    return render_template('manage-giftcards.html')
+    sql_giftcards = """
+        SELECT 
+            GiftCards.product_id, 
+            GiftCards.gift_card_id, 
+            GiftCards.code, 
+            GiftCards.balance, 
+            Orders.order_date,
+            Orders.user_id,
+            Products.description,
+            CONCAT(Consumer.given_name, ' ', Consumer.family_name) AS full_name 
+        FROM 
+            GiftCards
+        JOIN 
+            Orders ON GiftCards.order_id = Orders.order_id
+        JOIN 
+            Products ON GiftCards.product_id = Products.product_id
+        JOIN
+            Consumer ON Orders.user_id = Consumer.user_id;
+    """
+    gift_cards = fetchAll(sql_giftcards, None, True)
+    return render_template('manage-giftcards.html', giftcards=gift_cards)
 
 
-@app.route("/Giftcard/add",methods = ["POST"])
-@roleRequired(['National_Manager'])
-def addGiftcard():
-    return render_template('manage-giftcards.html')
