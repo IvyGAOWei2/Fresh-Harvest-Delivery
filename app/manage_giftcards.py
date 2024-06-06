d1d3e1f05727d6209473ccd0a4aad240ef71e3ab
Wei Gao <Wei.Gao2@lincolnuni.ac.nz>
2024-06-06 14:12:05 +1200

    system manage

--- app/manage_giftcards.py
+++ app/manage_giftcards.py
@@ -11,24 +11,39 @@ from common import roleRequired, validateProductProfile
 def manageGiftcard():
     sql_giftcards = """
         SELECT 
+            gc.balance,
+            p.description,
+            pi.image
+        FROM 
+            GiftCards gc
+        JOIN 
+            Products p ON gc.product_id = p.product_id
+        JOIN
+            ProductImages pi ON p.product_id = pi.product_id
+        WHERE 
+            pi.is_primary = TRUE
+        GROUP BY 
+            gc.balance, p.description, pi.image;
+    """
+    gift_cards = fetchAll(sql_giftcards, None, True)
+
+    sql_giftcards_redeem = """
+        SELECT 
             GiftCards.product_id, 
             GiftCards.gift_card_id, 
             GiftCards.code, 
             GiftCards.balance, 
             Orders.order_date,
             Orders.user_id,
-            Products.description,
             CONCAT(Consumer.given_name, ' ', Consumer.family_name) AS full_name 
         FROM 
             GiftCards
         JOIN 
             Orders ON GiftCards.order_id = Orders.order_id
-        JOIN 
-            Products ON GiftCards.product_id = Products.product_id
         JOIN
             Consumer ON Orders.user_id = Consumer.user_id;
     """
-    gift_cards = fetchAll(sql_giftcards, None, True)
-    return render_template('manage-giftcards.html', giftcards=gift_cards)
+    gift_cards_redeem = fetchAll(sql_giftcards_redeem, None, True)
+    return render_template('manage-giftcards.html',giftcards=gift_cards,  giftcardsRedeem=gift_cards_redeem)
 
 
--- app/manage_system_settings.py
+++ app/manage_system_settings.py
@@ -13,6 +13,36 @@ def manageSettings():
     return render_template('manage-system-settings.html', categories = app.category_list, units = app.unit_list)
 
 
+@app.route('/shipping/update', methods=['POST'])
+def updateShipping():
+    # for depot in shipping_prices.keys():
+    #     new_price = request.form.get(f'price_{depot}')
+    #     if new_price:
+    #         shipping_prices[depot] = float(new_price)
+    return redirect(url_for('manageSettings'))
+
+@app.route('/units/update', methods=['POST'])
+def updateUnits():
+    new_unit = request.form.get('new_unit')
+    # if new_unit:
+    #     units.append(new_unit)
+    return redirect(url_for('manageSettings'))
+
+@app.route('/units/add', methods=['POST'])
+def addUnits():
+    new_unit = request.form.get('new_unit')
+    # if new_unit:
+    #     units.append(new_unit)
+    return redirect(url_for('manageSettings'))
+
+
+@app.route('/category/add', methods=['POST'])
+def addCategories():
+    new_category = request.form.get('new_category')
+    # if new_category:
+    #     categories.append(new_category)
+    return redirect(url_for('manageSettings'))
+
 @app.route('/update/categories', methods=['POST'])
 def updateCategories():
     new_category = request.form.get('new_category')
@@ -20,33 +50,9 @@ def updateCategories():
     #     categories.append(new_category)
     return redirect(url_for('manageSettings'))
 
-@app.route('/add/category', methods=['POST'])
-def addCategories():
-    new_category = request.form.get('new_category')
-    # if new_category:
-    #     categories.append(new_category)
-    return redirect(url_for('manageSettings'))
 
 
-@app.route('/update/units', methods=['POST'])
-def updateUnits():
-    new_unit = request.form.get('new_unit')
-    # if new_unit:
-    #     units.append(new_unit)
-    return redirect(url_for('manageSettings'))
 
-@app.route('/add/units', methods=['POST'])
-def addUnits():
-    new_unit = request.form.get('new_unit')
-    # if new_unit:
-    #     units.append(new_unit)
-    return redirect(url_for('manageSettings'))
 
-@app.route('/update/shipping', methods=['POST'])
-def updateShipping():
-    # for depot in shipping_prices.keys():
-    #     new_price = request.form.get(f'price_{depot}')
-    #     if new_price:
-    #         shipping_prices[depot] = float(new_price)
-    return redirect(url_for('manageSettings'))
+
 
