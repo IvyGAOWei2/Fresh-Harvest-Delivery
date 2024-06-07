32f71f15cf56af246213a7f47bfd7be42d2ff806
HuizhongWang <minasopor23@gmail.com>
2024-06-07 20:23:24 +1200

    manage points

--- app/consumer_points.py
+++ app/consumer_points.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import app
 from flask import render_template, request, session
 
@@ -49,15 +50,61 @@ def managePoints():
             FROM Consumer u JOIN LatestPoints lp ON u.user_id = lp.user_id
             JOIN ConsumerPoints p ON p.point_id = lp.latest_point_id
             JOIN Depots on Depots.depot_id = u.depot_id;""", val=None, withDescription=False)
-    print(points_list)
 
-    detail_list = fetchOne("select * from ConsumerPoints;",val=None, withDescription=False)
-    print(detail_list )
-    
-    if request.method == ['POST']:
-        user_id =  request.form.get('user_id') 
-        point_id = request.form.get('point_id') 
-       
-        print(user_id,1111111111111)
 
     return render_template('manage_points.html', type=type, points_list=points_list)
+
+@app.route("/manage/points/details", methods=['POST'])
+@roleRequired(['Local_Manager', 'National_Manager'])
+def managePointsDetails():
+    type = session['type']
+    user_id = request.form.get('user_id')
+    action = request.form.get('action')
+
+    if action == 'view_details':
+        # 根据user_id查询表A的详细信息
+        user_details = fetchAll("SELECT * FROM ConsumerPoints WHERE user_id = %s", (user_id,), withDescription=False)
+        print(user_details)
+
+        points_list = fetchAll("""WITH LatestPoints AS 
+                (SELECT user_id, MAX(point_id) AS latest_point_id FROM ConsumerPoints GROUP BY user_id)
+                SELECT u.given_name,u.family_name,u.depot_id,
+                    Depots.location,           
+                    p.*  
+                FROM Consumer u JOIN LatestPoints lp ON u.user_id = lp.user_id
+                JOIN ConsumerPoints p ON p.point_id = lp.latest_point_id
+                JOIN Depots on Depots.depot_id = u.depot_id;""", val=None, withDescription=False)
+        
+        return render_template('manage_points.html', type=type, user_details=user_details,show_modal=True, points_list=points_list)
+    
+    elif action == 'update_points':
+        updatepoints = request.form.get('updatepoints')
+        current_date = datetime.now().date()
+        insertSQL("insert into ConsumerPoints(point_id, user_id,point_type,point_balance,point_date) values(default,%s,'Placeholder1',%s,%s)", (user_id,updatepoints,current_date))
+
+        # 重新获取列表数据
+        points_list = fetchAll("""WITH LatestPoints AS 
+                 (SELECT user_id, MAX(point_id) AS latest_point_id FROM ConsumerPoints GROUP BY user_id)
+                SELECT u.given_name,u.family_name,u.depot_id,
+                    Depots.location,           
+                    p.*  
+                FROM Consumer u JOIN LatestPoints lp ON u.user_id = lp.user_id
+                JOIN ConsumerPoints p ON p.point_id = lp.latest_point_id
+                JOIN Depots on Depots.depot_id = u.depot_id;""", val=None, withDescription=False)
+        
+        return render_template('manage_points.html', type=type, points_list=points_list)
+    else:
+        points_list = fetchAll("""WITH LatestPoints AS 
+                 (SELECT user_id, MAX(point_id) AS latest_point_id FROM ConsumerPoints GROUP BY user_id)
+                SELECT u.given_name,u.family_name,u.depot_id,
+                    Depots.location,           
+                    p.*  
+                FROM Consumer u JOIN LatestPoints lp ON u.user_id = lp.user_id
+                JOIN ConsumerPoints p ON p.point_id = lp.latest_point_id
+                JOIN Depots on Depots.depot_id = u.depot_id;""", val=None, withDescription=False)
+        
+        return render_template('manage_points.html', points_list=points_list, error="Unknown action")
+
+
+   
+    
