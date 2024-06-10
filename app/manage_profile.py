4e453bc895b3668067f7d369d4e4b76a52ee2b97
HuizhongWang <minasopor23@gmail.com>
2024-06-10 13:21:26 +1200

    manage profile depot

--- app/manage_profile.py
+++ app/manage_profile.py
@@ -31,23 +31,37 @@ def saveImage(img):
 @roleRequired(['Staff', 'Local_Manager', 'National_Manager'])
 def adminProfiles():
     type = session['type']
-    print(type,88888888)
+    depot_id = session['depot_id']
     profile_type = request.args.get('profile_type')
-    if profile_type == "Consumer":
-        result = fetchAll("SELECT Users.email, Consumer.* FROM Consumer \
-            JOIN Users on Consumer.user_id=Users.user_id WHERE Users.type='Consumer' AND Users.is_deleted = FALSE;",None ,True)
-    else:
-        if session.get('type') in ['Local_Manager']:
-            result = fetchAll("""SELECT Users.email, Users.type, Employees.* FROM Employees \
-                JOIN Users on Employees.user_id=Users.user_id WHERE Users.type='Staff' AND Users.is_deleted = FALSE;""",None ,True)
+    if depot_id == 6:
+        if profile_type == "Consumer":
+            result = fetchAll("SELECT Users.email, Consumer.* FROM Consumer \
+                JOIN Users on Consumer.user_id=Users.user_id WHERE Users.type='Consumer' AND Users.is_deleted = FALSE;",None ,True)
         else:
-            result = fetchAll("""SELECT Users.email,Users.type, Employees.* FROM Employees \
-                JOIN Users ON Employees.user_id = Users.user_id \
-                WHERE (Users.type = 'Staff' OR Users.type = 'Local_Manager') AND Users.is_deleted = FALSE;""",None ,True)
-            
+            if session.get('type') in ['Local_Manager']:
+                result = fetchAll("""SELECT Users.email, Users.type, Employees.* FROM Employees \
+                    JOIN Users on Employees.user_id=Users.user_id WHERE Users.type='Staff' AND Users.is_deleted = FALSE;""",None ,True)
+            else:
+                result = fetchAll("""SELECT Users.email,Users.type, Employees.* FROM Employees \
+                    JOIN Users ON Employees.user_id = Users.user_id \
+                    WHERE (Users.type = 'Staff' OR Users.type = 'Local_Manager') AND Users.is_deleted = FALSE;""",None ,True)
+    else:
+        if profile_type == "Consumer":
+            result = fetchAll("SELECT Users.email, Consumer.* FROM Consumer \
+                JOIN Users on Consumer.user_id=Users.user_id WHERE Users.type='Consumer' AND Users.depot_id=%s AND Users.is_deleted = FALSE;",(depot_id,),True)
+        else:
+            if session.get('type') in ['Local_Manager']:
+                result = fetchAll("""SELECT Users.email, Users.type, Employees.* FROM Employees \
+                    JOIN Users on Employees.user_id=Users.user_id WHERE Users.type='Staff' AND Users.depot_id=%s AND Users.is_deleted = FALSE;""",(depot_id,),True)
+            else:
+                result = fetchAll("""SELECT Users.email,Users.type, Employees.* FROM Employees \
+                    JOIN Users ON Employees.user_id = Users.user_id \
+                    WHERE (Users.type = 'Staff' OR Users.type = 'Local_Manager') AND Users.depot_id=%s AND Users.is_deleted = FALSE;""",(depot_id,),True)
+
+                
     invoice = fetchAll("SELECT * from Invoices;",None ,True)
 
-    return render_template('admin_profile_list.html', member_list=result, profile_type=profile_type, depotList=app.depot_list, type=type,invoice=invoice )
+    return render_template('admin_profile_list.html', member_list=result, profile_type=profile_type, depotList=app.depot_list, type=type,invoice=invoice,depot_id=depot_id)
 
 
 @app.route('/admin/profile/search',methods = ["GET","POST"])
